9c7ab9eff1aed4e53544b3a135d5c07229cfa7e5
erey21 <arinerey@yandex.ru>
2021-11-04 22:27:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homework_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание № 9");
+
+            Console.WriteLine("Введите сумму вклада: ");
+            double sum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите количество месяцев: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            while (count < month) {
+                sum += sum * 0.07;
+                count++;
+            }
+
+            Console.WriteLine($"Итоговая сумма вклада: {sum}");
+
+        }
+    }
+}
